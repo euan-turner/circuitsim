c77d8c79af44dffb0135bc2022159cdfdf8c87e2
Euan Turner <euanoturner@gmail.com>
2023-07-09 16:58:13 +0100

    Reordered structs to optimise for memory

--- typedefs.h
+++ typedefs.h
@@ -8,27 +8,27 @@ typedef bool (*logic_op)(bool val1, bool val2);
 typedef struct logic_gate *logic_gate;
 typedef struct logic_gate *logic_input;
 struct logic_gate {
-  logic_gate input1;
-  logic_gate input2;
-  logic_op op;
   bool value;
   bool defined;
   char *label;
+  logic_gate input1;
+  logic_gate input2;
+  logic_op op;
   int fan_out;
 };
 
 typedef struct logic_output *logic_output;
 struct logic_output {
-  logic_gate gate;
   char *label;
+  logic_gate gate;
 };
 
 typedef struct circuit *circuit;
 struct circuit {
-  logic_input *inputs;
   int num_inputs;
-  logic_output *outputs;
   int num_outputs;
+  logic_input *inputs;
+  logic_output *outputs;
 };
 
 typedef enum { GIVEN, ALL } input_type;
